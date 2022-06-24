25ae4eb41d3e095e5aeb6854925859a7f01998f1
fadhilihsn <email address>
2022-06-24 21:28:06 +0700

    Updates

---
+++ precache-manifest.6778d6f1f95798f700f9c1fd89736cf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef4d1dd3fb9633280326",
+    "url": "/Todolist/static/css/main.71ce13b2.chunk.css"
+  },
+  {
+    "revision": "ef4d1dd3fb9633280326",
+    "url": "/Todolist/static/js/main.ef4d1dd3.chunk.js"
+  },
+  {
+    "revision": "f917524f92675769f5f4",
+    "url": "/Todolist/static/js/1.f917524f.chunk.js"
+  },
+  {
+    "revision": "bf388b0f515924e67137",
+    "url": "/Todolist/static/js/runtime~main.bf388b0f.js"
+  },
+  {
+    "revision": "4134429784a566ecb0252ff8030c0563",
+    "url": "/Todolist/index.html"
+  }
+];
